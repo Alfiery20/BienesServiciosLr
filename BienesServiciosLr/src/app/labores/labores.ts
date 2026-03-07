06620c04c18ea8d67c77716cf1bda1fc1881780f
Alfiery <alfie@ALF1ERY>
2026-03-07 00:43:38 -0500

    Estructura inicial

---
+++ BienesServiciosLr/src/app/labores/labores.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labores',
+  imports: [],
+  templateUrl: './labores.html',
+  styleUrl: './labores.css',
+})
+export class Labores {}
